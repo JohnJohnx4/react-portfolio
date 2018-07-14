bda7632c46654676f81d58e601a8f69d479e516f
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-07-14 11:20:17 -0400

    changes to about and home

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -52,13 +52,10 @@ function Home(props) {
               />
             </div>
             <Typography className={classes.heading + " home__header"}>
-              Hi! My name is John, and I am a web developer. I have over 8 years
-              of experience in the 3 major theme parks in Orlando, and I am an
-              expert at customer service. I also enjoy game development as a
-              hobby.
+              Hi! My name is John, and I am a web developer. I have worked in Theme Parks and customer service for the past eight years, and I have really enjoyed my experiences. I knew I definitely wanted to work with computers more and have been in constant pursuit of getting the education neccesary to start my career. I have moved around different jobs, sometimes holding two or three, trying to make a living while learning as much as I can to be a good programmer. My journey has finally led me to this point,and I now have the skills to be part of the tech industry and start my next adventure!
             </Typography>
             <Typography className={classes.secondaryHeading + " home__subheader"}>
-              Please explore my site to learn more or click on the icon on the
+              Please explore my site to learn more about me and what I have done or click on the icon on the
               bottom-right to download my resume, access my GitHub Repos, and
               visit my LinkedIn.
             </Typography>
--- portfolio/src/components/Projects/List.js
+++ portfolio/src/components/Projects/List.js
@@ -38,7 +38,7 @@ function SimpleList(props) {
             <SprayIcon />
           </ListItemIcon >
           <ListItemText className="project__title" primary="Hairspray" />
-          <Typography className="project__text">An app for hair salon guests to book appointments. Built with Mern and utilizes Twilio and Stripe APIs</Typography>
+          <Typography className="project__text">An app for hair salon guests to book appointments. Built with MERN and utilizes Twilio and Stripe APIs</Typography>
         </ListItem>
 
                 <a href="https://johnc-noter.herokuapp.com">
@@ -47,17 +47,17 @@ function SimpleList(props) {
               <NoteMultipleOutlineIcon />
             </ListItemIcon >
             <ListItemText className="project__title" primary="Noter" />
-            <Typography className="project__text">A note taking app able to CRUD simple notes. Made with MERN, deployed to Heroku</Typography>
+            <Typography className="project__text">A note taking app able to create, read, update, and delete simple notes. Made with MERN, deployed to Heroku</Typography>
           </ListItem>
         </a>
 
-        <ListItem button component="a" href="https://johnc-djorg.herokuapp.com">
+        {/* <ListItem button component="a" href="https://johnc-djorg.herokuapp.com">
           <ListItemIcon className="project__icon">
             <BookmarkOutlineIcon />
           </ListItemIcon >
           <ListItemText className="project__title" primary="Djorg" />
           <Typography className="project__text">A bookmarking app built with Python and Django</Typography>
-        </ListItem>
+        </ListItem> */}
 
         <ListItem
           button
@@ -68,7 +68,7 @@ function SimpleList(props) {
             <GamepadVariantIcon />
           </ListItemIcon >
           <ListItemText className="project__title" primary="Shattered Iron" />
-          <Typography className="project__text">A 'Twisted Metal' game clone, built with Unity and deployed to Heroku in only 30 hours</Typography>
+          <Typography className="project__text">A 'Twisted Metal' game clone, built with Unity and deployed to Heroku in under only 30 hours</Typography>
         </ListItem>
 
         <ListItem
@@ -80,7 +80,7 @@ function SimpleList(props) {
             <GunIcon />
           </ListItemIcon >
           <ListItemText className="project__title" primary="Zombie Killers" />
-          <Typography className="project__text">A zombie FPS made in Unity in less than 30 hours. </Typography>
+          <Typography className="project__text">A zombie first-person shooter made in Unity in less than 30 hours. </Typography>
         </ListItem>
 
         <ListItem
