0852a9f699a49242f3b29f35673d74b0e634dda6
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-08-09 19:32:30 -0400

    added server and password model

--- server/app.js
+++ server/app.js
@@ -1,50 +1,52 @@
-require('dotenv').load();
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").load();
+const express = require("express");
+const mongoose = require("mongoose");
 // const helmet = require('helmet');
-const morgan = require('morgan');
-const cors = require('cors');
-const path = require('path');
-
-
-const routes = require('./routes/');
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+const password = require('./controllers/passwordController');
+// const routes = require('./routes/');
 // const keys = require('./config/keys');
+
 const port = process.env.PORT || 5000;
 const app = express();
 
-const scheduler = require('./scheduler.js');
-
 // Connecting to mLab/port
 mongoose
-  .connect(
-    process.env.DATA_BASE,
-    { useNewUrlParser: true }
-  )
-  .then(() => {
-    console.log('Connected to DB');
-  })
-  .catch(err => {
-    console.log('Error connecting to DB', err);
-  });
+	.connect(
+		process.env.DATA_BASE || "mongodb://localhost:27017/portfolio",
+		{ useNewUrlParser: true }
+	)
+	.then(() => {
+		console.log("Connected to DB");
+	})
+	.catch(err => {
+		console.log("Error connecting to DB", err);
+	});
 
 app.listen(port, err => {
-  console.log(`connected to the server port ${port}`);
+	console.log(`connected to the server port ${port}`);
 });
 
 // Middleware
-app.use(morgan('dev'));
-app.use(express.json());
 // app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
 app.use(cors());
 
-// Unncomment to have scheduler run continuously
-scheduler.start();
-
 // Routes
-routes(app);
+// routes(app);
+app.get("/", (req, res) => {
+	res.status(200).json("Hey, this is my server!");
+});
+
+app.route("/api/capstone/register/password").post(password.POST);
+
 
 // For Heroku Build
-app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../', 'client', 'build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, '../client/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../', 'client', 'build', 'index.html'));
+// });
+
