486c0ece4fb8cc574e8da49721776e31de5c5443
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-08-17 17:54:38 -0400

    fixed subtext for home page

--- portfolio/src/components/Home/AboutText.js
+++ portfolio/src/components/Home/AboutText.js
@@ -14,4 +14,4 @@ const newText = "Hello! My name is John and I am a software engineer hobbyist go
 
 // const oldText = "Hi! My name is John, and I am a web developer. I have worked in Theme Parks and customer service for the past eight years, and I have really enjoyed my experiences. I knew I definitely wanted to work with computers more and have been in constant pursuit of getting the education neccesary to start my career. I have moved around different jobs, sometimes holding two or three, trying to make a living while learning as much as I can to be a good programmer. My journey has finally led me to this point,and I now have the skills to be part of the tech industry and start my next adventure!";
 
-const oldSubText = "Please explore my site to learn more about me and what I have done or click on the icon on the bottom-right to download my resume, access my GitHub Repos, and visit my LinkedIn."
+const oldSubText = "Please explore my site to learn more about me and my project. If you would like to contact me or see my resume, please go to my Contact page."
