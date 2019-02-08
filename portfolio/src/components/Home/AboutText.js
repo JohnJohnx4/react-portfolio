9915d36b5eca7db267f6f7d90d268d51533601a4
John C <33876234+JohnJohnx4@users.noreply.github.com>
2019-02-08 15:28:08 -0500

    updated home text

--- portfolio/src/components/Home/AboutText.js
+++ portfolio/src/components/Home/AboutText.js
@@ -8,10 +8,6 @@ export function AboutSubText() {
 	// return newSubText;
 }
 
-const newText = "Hello! My name is John and I am a software engineer hobbyist going professional. I love building games with C# and Unity, but I can build just about anything (JS, React, Node, Express, Python, Django, and more)!";
-
-// const newSubText = ""
-
-// const oldText = "Hi! My name is John, and I am a web developer. I have worked in Theme Parks and customer service for the past eight years, and I have really enjoyed my experiences. I knew I definitely wanted to work with computers more and have been in constant pursuit of getting the education neccesary to start my career. I have moved around different jobs, sometimes holding two or three, trying to make a living while learning as much as I can to be a good programmer. My journey has finally led me to this point,and I now have the skills to be part of the tech industry and start my next adventure!";
+const newText = "Hello! My name is John and I am a professional software engineer. I have experience with React, Angular, Node, Firebase, MongoDB, and more!";
 
 const oldSubText = "Please explore my site to learn more about me and my project. If you would like to contact me or see my resume, please go to my Contact page."
