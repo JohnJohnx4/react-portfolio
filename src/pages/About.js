b9b0e96265ff628fe4fec5ff007bb949312b16d7
John Correia <johnjohnx4@gmail.com>
2021-02-12 13:37:25 -0500

    about page completed

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,78 @@
-const AboutPage = () => <div>This is the about page</div>;
+import { Container, Row, Col, Card, Media } from 'reactstrap';
+import ProfileImg1 from '../assets/profile1.jpg';
+import ProfileImg2 from '../assets/profile2.jpg';
+
+const AboutPage = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Card className='p-5 mt-5'>
+            <Media>
+              <Media left>
+                <Media
+                  className='mx-3'
+                  object
+                  src={ProfileImg1}
+                  alt='Generic placeholder image'
+                  style={{ height: '300px', width: 'auto' }}
+                />
+              </Media>
+              <Media body className='ml-5 mt-2'>
+                <Media heading className='mx-5'>
+                  About Me
+                </Media>
+                <Media className='mt-5 ml-5'>
+                  My name is John P. Correia, and I have been a professional Web
+                  Developer for more than two years now. I enjoy creating user
+                  interfaces in React, developing APIs in Node and Express, and
+                  utilizing MongoDB, GraphQL, and MySQL databases. I have a
+                  large amount of experience turning design docs into functional
+                  apps.
+                  <br />
+                  <br />I have worked mainly for startups and am very
+                  comfortable in that fast-paced environment. Tools I have used
+                  include Github, Bitbucket, Jira, Trello, and I am comfortable
+                  in both Windows and macOS environments. I am comfortable in
+                  React and Javascript but can pick up new frameworks and
+                  languages quickly.
+                </Media>
+              </Media>
+            </Media>
+            <Media className='mt-5'>
+              <Media body className='mx-3 mt-2'>
+                <Media heading>Media heading</Media>
+                <Media className='mt-5 mr-5'>
+                  In my spare time, I enjoy playing games online with friends,
+                  streaming on Twitch occasionally. I also enjoy watching anime
+                  and tv shows. I try to stay active by working out in my garage
+                  gym, lifting weights. When I have time, I like to fiddle with
+                  Unity and aspire to create my own game one day.
+                  <br />
+                  <br />
+                  I also play trumpet in a community orchestra and jazz band
+                  every Sunday evening. I love playing music and make time to
+                  practice outside of rehearsal as often as I can.
+                  <br />
+                  <br />
+                  Thank you for taking the time to read about me!
+                </Media>
+              </Media>
+              <Media right>
+                <Media
+                  className='mx-3'
+                  object
+                  src={ProfileImg2}
+                  alt='Generic placeholder image'
+                  style={{ height: '300px', width: 'auto' }}
+                />
+              </Media>
+            </Media>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default AboutPage;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -15,39 +15,43 @@ import FundopolisImg from '../assets/fundopolis.png';
 import ShatteredImg from '../assets/shattered.png';
 import NoterImg from '../assets/noter.png';
 
-const items = [
-  {
-    src: FundopolisImg,
-    altText: 'Fundopolis website screepcap',
-    caption: '',
-    description: 'About page built with GatsbyJS and Ghost CMS. Deployed with AWS using Lightsail and ',
-    href: 'https://about.fundopolis.com/'
-  },
-  {
-    src: NoterImg,
-    altText: '',
-    caption: '',
-    description: 'Note taking application built with React. Styled with Material-UI and deployed to Heroku',
-    href: 'https://johnc-noter.herokuapp.com/'
-  },
-  // {
-  //   src: HairsprayImg,
-  //   altText: '',
-  //   caption: '',
-  //   description: 'Full Stack Application used to book appointments. React, Node, Mongo, deployed to Heroku and Netlify',
-  //   href: '#'
-  // },
-  {
-    src: ShatteredImg,
-    altText: '',
-    caption: '',
-    description: 'Video Game built with Unity for a hackathon. Deployed to Heroku. (Resource intensive, takes time to load)',
-    href: 'https://shattered-iron.herokuapp.com/'
-  },
-];
-
 const MainPage = () => {
-  const [currentSlide, setCurrentSlide] = useState(0)
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const items = [
+    {
+      src: FundopolisImg,
+      altText: 'Fundopolis website screepcap',
+      caption: '',
+      description:
+        'About page built with GatsbyJS and Ghost CMS. Deployed with AWS using Lightsail and ',
+      href: 'https://about.fundopolis.com/',
+    },
+    {
+      src: NoterImg,
+      altText: '',
+      caption: '',
+      description:
+        'Note taking application built with React. Styled with Material-UI and deployed to Heroku',
+      href: 'https://johnc-noter.herokuapp.com/',
+    },
+    // {
+    //   src: HairsprayImg,
+    //   altText: '',
+    //   caption: '',
+    //   description: 'Full Stack Application used to book appointments. React, Node, Mongo, deployed to Heroku and Netlify',
+    //   href: '#'
+    // },
+    {
+      src: ShatteredImg,
+      altText: '',
+      caption: '',
+      description:
+        'Video Game built with Unity for a hackathon. Deployed to Heroku. (Resource intensive, takes time to load)',
+      href: 'https://shattered-iron.herokuapp.com/',
+    },
+  ];
+
   return (
     <Container>
       <Row>
@@ -59,7 +63,7 @@ const MainPage = () => {
                 building apps in React, Node, GraphQL, and much more. Please
                 check out my projects!
               </CardTitle>
-              <Carousel items={items} setCurrentSlide={setCurrentSlide}/>
+              <Carousel items={items} setCurrentSlide={setCurrentSlide} />
             </CardBody>
             <CardBody>
               <CardText>{items[currentSlide].description}</CardText>
