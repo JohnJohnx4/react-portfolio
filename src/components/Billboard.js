23e0796b3a32cebcdc8779ef8ac3272595722384
John Correia <johnjohnx4@gmail.com>
2021-02-12 19:46:49 -0500

    completed contact section

---
+++ src/components/Billboard.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Jumbotron, Button } from 'reactstrap';
+
+const Billboard = (props) => {
+  return (
+    <div>
+      <Jumbotron className='py-5'>
+        <h1 className='display-5'>I am currently looking for work!</h1>
+        <p className='lead'>Are you looking for a full-stack developer?</p>
+        <hr className='my-2' />
+        <p>
+          I am seeking out a new oppurtunity to utilize my skills as a web
+          developer in order to build out projects with clean, maintainable
+          code. Please do not hesitate to contact me with your next project!
+        </p>
+        <p className='mt-4'>
+          <Button
+            className='mr-2 mt-1'
+            outline 
+            variant='link'
+            color='primary'
+            style={{minWidth: '120px'}}
+            href='https://drive.google.com/file/d/1_iMU5bTAD9vc7q0MyG6soGdND9IugVmT/view?usp=sharing'
+            target='_blank'
+          >
+            Resume
+          </Button>
+          <Button
+            className='mr-2 mt-1'
+            outline 
+            variant='link'
+            color='primary'
+            style={{minWidth: '120px'}}
+            href='mailto:johnpaulcorreia@gmail.com'
+          >
+            Email
+          </Button>
+          <Button
+            className='mr-2 mt-1'
+            outline 
+            variant='link'
+            color='primary'
+            style={{minWidth: '120px'}}
+            target='_blank'
+            href='https://github.com/JohnJohnx4'
+          >
+            Github
+          </Button>
+          <Button
+            className='mr-2 mt-1'
+            outline 
+            variant='link'
+            color='primary'
+            style={{minWidth: '120px'}}
+            target='_blank'
+            href='https://www.linkedin.com/in/john-correia/'
+          >
+            LinkedIn
+          </Button>
+        </p>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default Billboard;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,23 @@
-const ContactPage = () => <div>This is the Contact page</div>;
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardTitle,
+  CardText,
+  CardBody,
+  CardLink,
+} from 'reactstrap';
+import Billboard from '../components/Billboard';
+
+const ContactPage = () => {
+  return (
+    <Container className='px-5 pt-5 mt-5'>
+      <Row>
+        <Billboard />
+      </Row>
+    </Container>
+  );
+};
 
 export default ContactPage;
