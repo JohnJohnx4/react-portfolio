9671ee4ebb611fcbb77fcc106ba64afcf7e11092
John Correia <johnjohnx4@gmail.com>
2021-02-15 16:50:08 -0500

    remove index css file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 
 import Layout from './layout/Layout';
 import MainPage from './pages/Main';
