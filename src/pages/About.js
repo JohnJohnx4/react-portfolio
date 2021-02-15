729bee9b82c4adc940151e272272a8363406a64d
John <johnc@fundopolis.com>
2021-02-15 15:22:39 -0500

    changes to about for responsive styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Card, Media } from 'reactstrap';
+import { Container, Row, Col, Card, Media, CardTitle, CardText } from 'reactstrap';
 import ProfileImg1 from '../assets/profile1.jpg';
 import ProfileImg2 from '../assets/profile2.jpg';
 
@@ -8,21 +8,17 @@ const AboutPage = () => {
       <Row>
         <Col>
           <Card className='p-5 my-5'>
-            <Media>
-              <Media left>
-                <Media
-                  className='mx-3'
-                  object
-                  src={ProfileImg1}
-                  alt='Generic placeholder image'
-                  style={{ height: '300px', width: 'auto' }}
-                />
-              </Media>
-              <Media body className='ml-5 mt-2'>
-                <Media heading className='mx-5'>
+                
+                <CardTitle className='mx-5'>
                   About Me
-                </Media>
-                <Media className='mt-5 ml-5'>
+                </CardTitle>
+                <div>
+                
+                <div className='w-25 mr-5'>
+                  <img src={ProfileImg1} className='img-fluid img-thumbnail float-left'/>
+                </div>
+                <div className='ml-5'>
+                <p>
                   My name is John P. Correia, and I have been a professional Web
                   Developer for more than two years now. I enjoy creating user
                   interfaces in React, developing APIs in Node and Express, and
@@ -36,13 +32,8 @@ const AboutPage = () => {
                   in both Windows and macOS environments. I am comfortable in
                   React and Javascript but can pick up new frameworks and
                   languages quickly.
-                </Media>
-              </Media>
-            </Media>
-            <Media className='mt-5'>
-              <Media body className='mx-3 mt-2'>
-                <Media heading>Media heading</Media>
-                <Media className='mt-5 mr-5'>
+                  <br />
+                  <br />
                   In my spare time, I enjoy playing games online with friends,
                   streaming on Twitch occasionally. I also enjoy watching anime
                   and tv shows. I try to stay active by working out in my garage
@@ -56,18 +47,9 @@ const AboutPage = () => {
                   <br />
                   <br />
                   Thank you for taking the time to read about me!
-                </Media>
-              </Media>
-              <Media right>
-                <Media
-                  className='mx-3'
-                  object
-                  src={ProfileImg2}
-                  alt='Generic placeholder image'
-                  style={{ height: '300px', width: 'auto' }}
-                />
-              </Media>
-            </Media>
+                </p>
+                </div>
+                </div>
           </Card>
         </Col>
       </Row>
